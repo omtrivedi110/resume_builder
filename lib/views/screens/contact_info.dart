78604dc9eafe562eb1b48ec279eda1d5a729c4d8
omtrivedi110 <ptrivedi460@gmail.com>
2023-04-20 22:29:19 +0530

    om | 20 - 04 - 2023 11 : 00

--- lib/views/screens/contact_info.dart
+++ lib/views/screens/contact_info.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../utils/backbutton.dart';
+import '../../utils/icons_utils.dart';
 import '../../utils/textstyle.dart';
 
 class contact_info extends StatefulWidget {
@@ -11,81 +12,115 @@ class contact_info extends StatefulWidget {
 }
 
 class _contact_infoState extends State<contact_info> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
   int index = 0;
+  GlobalKey namekey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          leading: Mybackicon(),
-          title: Text(
-            "Reference",
-            style: titletextstyle,
-          ),
-          backgroundColor: Colors.orangeAccent,
-          centerTitle: true,
+      appBar: AppBar(
+        leading: Mybackicon(),
+        title: Text(
+          "Contact Info",
+          style: titletextstyle,
         ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        index = 0;
-                      });
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(
-                        color: Colors.orangeAccent,
-                        border: Border(
-                          bottom: BorderSide(),
-                        ),
-                      ),
-                      height: s.height * 0.10,
-                      child: const Text("Contact Details",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        index = 1;
-                      });
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
+        backgroundColor: Colors.orangeAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      index = 0;
+                    });
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: s.height * 0.12,
+                    decoration: BoxDecoration(
                       color: Colors.orangeAccent,
-                      height: s.height * 0.10,
-                      child: const Text("Profile Photo",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
+                    ),
+                    child: Text(
+                      "Contact Detail",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
                   ),
                 ),
-              ],
-            ),
-            IndexedStack(
-              index: index,
-              children: [
-                Text("this is Contact"),
-                Text("this is photo"),
-              ],
-            ),
-          ],
-        ));
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      index = 1;
+                    });
+                  },
+                  child: Container(
+                    height: s.height * 0.12,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.orangeAccent,
+                    ),
+                    child: Text(
+                      "Profile Photo",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          IndexedStack(
+            index: index,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  // height: 5,
+                  width: s.width,
+                  color: Colors.white,
+                  child: Form(
+                    key: formkey,
+                    child: Column(
+                      // mainAxisSize: MainAxisSize.max,
+                      children: [
+                        // Text("hello")
+                        Row(
+                          children: [
+                            Image.asset(
+                              "${icons}user.png",
+                              height: 20,
+                            ),
+                            TextFormField(
+                              // key: formkey,
+                              key: namekey,
+                              decoration: const InputDecoration(
+                                  hintText: "Enter Name",
+                                  border: UnderlineInputBorder()),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      backgroundColor: Colors.grey,
+    );
   }
 }
